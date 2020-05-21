a2967269624405bfa71f791ba65f0d1a8a1a4167
Solveig Hansen <supersolveig@gmail.com>
2020-05-21 22:48:19 +0200

    Add some more comments to solution day 6 part 1

--- day6.py
+++ day6.py
@@ -1,4 +1,8 @@
+#
+#   Part 1
+#
 
+# Recursive function to count the number of direct and indirect orbits
 def countOrbits(key, dict, count):
     val = dict[key]
     if dict.get(val) == None:
@@ -6,6 +10,7 @@ def countOrbits(key, dict, count):
     else:
         return(countOrbits(val, dict, count + 1))
 
+# Use a dictionary to keep track of which is orbiting which
 orbits = {}
 with open("day6_puzzle.txt") as puzzle:
     # Each line represent a relationship, where one is the orbitee and
@@ -21,6 +26,8 @@ with open("day6_puzzle.txt") as puzzle:
         #print 'orbiter: ', orbiter
         orbits[orbiter] = orbitee
 
+# add up the number that each item in the dictionary is orbiting another object
+# either directly or indirectly
 numOrbits = 0
 for key in orbits:
     numOrbits += countOrbits(key, orbits, 1)
