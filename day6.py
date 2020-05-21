86a7f1bcd10659e6bf15044e338c0286cbae3136
Solveig Hansen <supersolveig@gmail.com>
2020-05-21 23:27:05 +0200

    Finished day 6

--- day6.py
+++ day6.py
@@ -33,3 +33,40 @@ for key in orbits:
     numOrbits += countOrbits(key, orbits, 1)
 
 print "Number of direct and indirect orbits: ", numOrbits
+
+#
+#   Part 2
+#
+
+# Create a list of the direct and indirect orbits from a starting position (key)
+def makeOrbitalTraversionMap(key, dict, map):
+    val = dict[key]
+    if (dict.get(val) == None):
+        return(map)
+    else:
+        map.append(val)
+        return(makeOrbitalTraversionMap(val, dict, map))
+
+# Find number of orbits between one position and another
+def countOrbitsFromTo(fromPos, toPos, dict, count):
+    val = dict[fromPos]
+    if dict.get(val) == toPos:
+        return(count)
+    else:
+        return(countOrbitsFromTo(val, toPos, dict, count + 1))
+
+# Map all the objects Santa orbits, as a path from outer to inner
+map1 = makeOrbitalTraversionMap("SAN", orbits, [])
+# and the same for yourself
+map2 = makeOrbitalTraversionMap("YOU", orbits, [])
+
+# Find where the two paths intersects, so that we know the position from
+# which to backtrack and count so that we can figure out how many orbital
+# transfers are needed to take for YOU to orbit around the same object as SAN
+intersections = [x for x in map1 if x in map2]
+firstIntersect = intersections[0]
+
+count1 = countOrbitsFromTo("SAN", firstIntersect, orbits, 1)
+count2 = countOrbitsFromTo("YOU", firstIntersect, orbits, 1)
+
+print "Number of orbital transfers: ", count1 + count2
