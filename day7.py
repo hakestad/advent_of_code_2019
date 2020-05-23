de088d3de400dba6575318331bb088d128cd6126
Solveig Hansen <supersolveig@gmail.com>
2020-05-23 15:08:27 +0200

    Solved day 7 part 1 and added gitignore file to prevent .pyc files from being committed to the project

---
+++ day7.py
@@ -0,0 +1,27 @@
+# Import intcode computer made on day5
+from intcode import amplifierOutputChecker
+import itertools
+
+program = [3,8,1001,8,10,8,105,1,0,0,21,30,55,80,101,118,199,280,361,442,99999,3,9,101,4,9,9,4,9,99,3,9,101,4,9,9,1002,9,4,9,101,4,9,9,1002,9,5,9,1001,9,2,9,4,9,99,3,9,101,5,9,9,1002,9,2,9,101,3,9,9,102,4,9,9,1001,9,2,9,4,9,99,3,9,102,2,9,9,101,5,9,9,102,3,9,9,101,3,9,9,4,9,99,3,9,1001,9,2,9,102,4,9,9,1001,9,3,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99]
+
+# First, setup all the possible combinations of phase sequences
+# Using the itertools package to produce sequences of 5 that contain
+# combinations of the values 0 to 4 (5 not included)
+allSequences = list(itertools.product(range(0, 5), repeat=5))
+# Filter out sequences from the above so that we only have sequencecs where all
+# values are unique - using python set function which doesn't allow for duplicates
+phaseSequences = []
+for seq in allSequences:
+    if len(set(seq)) == 5:
+        phaseSequences.append(list(seq))
+
+print "num phase sequences: ", len(phaseSequences)
+
+outputList = []
+amplifierOutputChecker(program, phaseSequences, outputList)
+print outputList
+
+highestOutput = max(outputList)
+highestOutputIndex = outputList.index(highestOutput)
+
+print "Highest output was: ", highestOutput, " and the sequence was: ", phaseSequences[highestOutputIndex]
---
+++ intcode.py
@@ -0,0 +1,125 @@
+#
+#   Reusing part of code from day 5
+#
+
+# @program  - List. Program to run
+# @outputList - List. Stores the outputs from the program
+# @inputType - String. defaults to user input, but can be set to "program" which
+#   means it will read from a list provided as either the phase or the outputList
+#   depending on an internal state
+# @phase - Int. used by day7. Will be the phase for one run of the program
+def intcode(program, outputList, inputType = "user", phase = 0):
+    opCode = 0
+    pointer = 0
+    # internal state that decided whether to use phase as "input" or last item in outputList as "input"
+    useInputPhase = True
+    while opCode != 99:
+        codes = [int(i) for i in str(program[pointer])]
+        numCodes = len(codes)
+
+        # The opCode is the two rightmost digits of the instruction, or the last digit for instructions
+        # that are lacking modes and are only single digit - handle this with ternary construct
+        # Need to combine them to a string to put them together, then re-convert back to int
+        opCode = int(`codes[numCodes - 2]` + `codes[numCodes - 1]`) if numCodes >= 2 else codes[0]
+
+        if opCode == 99:
+            break
+
+        # Defaults are mode 0 - position mode, else read mode from
+        # third last, fourth last or fifth last code if they exist
+        mode1 = codes[numCodes - 3] if numCodes >= 3 else 0
+        mode2 = codes[numCodes - 4] if numCodes >= 4 else 0
+        mode3 = codes[numCodes - 5] if numCodes >= 5 else 0
+
+        index1 = program[pointer + 1]
+        # Only setup indices for these if there are appropriate number of params for this opCode
+        index2 = program[pointer + 2] if opCode == 1 or opCode == 2 or opCode == 5 or opCode == 6 or opCode == 7 or opCode == 8 else None
+        index3 = program[pointer + 3] if opCode == 1 or opCode == 2 or opCode == 7 or opCode == 8 else None
+
+        if opCode == 1:
+            # Depending on the mode, you should either add the values at
+            # the indexed positions (mode == 0) together, or add the
+            # direct values (indices) together (mode == 1)
+            a1 = program[index1] if mode1 == 0 else index1
+            a2 = program[index2] if mode2 == 0 else index2
+            program[index3] = a1 + a2
+            # Increase pointer by 4, since this instruction uses three parameters
+            pointer += 4
+        elif opCode == 2:
+            # For code 2, you should multiply instead
+            m1 = program[index1] if mode1 == 0 else index1
+            m2 = program[index2] if mode2 == 0 else index2
+            program[index3] = m1 * m2
+            # Increase pointer by 4, since this instruction uses three parameters
+            pointer += 4
+        elif opCode == 3:
+            val = 0
+
+            if inputType == 'user':
+                # Take single input value and store that value at the position given by parameter
+                try:
+                    val = int(input("Input: "))
+                except ValueError:
+                    print "Not a number."
+            else:
+                if useInputPhase == True:
+                    val = phase
+                    # Set to false so that it uses last item in outputList next time
+                    useInputPhase = False
+                else:
+                    # If not, it means the last value of the outputList should be used
+                    val = outputList[len(outputList) - 1]
+                    # Set to true so that it uses phase next time
+                    useInputPhase = True
+
+            program[index1] = val
+            # Increase pointer by 2, since this instruction has only one parameter
+            pointer += 2
+        elif opCode == 4:
+            # Output value at the position given by the first parameter, or the index value if mode == 1
+            out = program[index1] if mode1 == 0 else index1
+            outputList.append(out)
+            print "Output: ", out
+
+            # Increase pointer by 2, since this instruction has only one parameter
+            pointer += 2
+        elif opCode == 5:
+            firstParam = program[index1] if mode1 == 0 else index1
+            if (firstParam != 0):
+                pointer = program[index2] if mode2 == 0 else index2
+            else:
+                pointer += 3
+        elif opCode == 6:
+            firstParam = program[index1] if mode1 == 0 else index1
+            if (firstParam == 0):
+                pointer = program[index2] if mode2 == 0 else index2
+            else:
+                pointer += 3
+        elif opCode == 7:
+            firstParam = program[index1] if mode1 == 0 else index1
+            secondParam = program[index2] if mode2 == 0 else index2
+            program[index3] = 1 if firstParam < secondParam else 0
+            pointer += 4
+        elif opCode == 8:
+            firstParam = program[index1] if mode1 == 0 else index1
+            secondParam = program[index2] if mode2 == 0 else index2
+            program[index3] = 1 if firstParam == secondParam else 0
+            pointer += 4
+    return(outputList)
+
+
+def amplifierOutputChecker(program, phaseSequences, outputList):
+    for seq in phaseSequences:
+        print "sequence: ", seq
+        # For each sequence, we reset the temporary list of outputs
+        tempOutputList = [0]
+        # and we reset the program so that each sequence combination will
+        # start alike
+        testProgram = program
+        for phase in seq:
+            print "phase", phase
+            tempOutputList = intcode(testProgram, tempOutputList, "program", phase)
+        # Save the last output to store to be able to compare
+        # which sequence yielded the highest output
+        outputList.append(tempOutputList[len(tempOutputList) - 1])
+    return outputList
