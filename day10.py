f51cb3b47089ecb1146fbe182ee57765aebdd2bf
Solveig Hansen <supersolveig@gmail.com>
2020-05-26 13:21:21 +0200

    Finished part 2 day10

--- day10.py
+++ day10.py
@@ -36,3 +36,64 @@ mostClearPaths = max(clearPathCounter)
 bestAstroidIndex = clearPathCounter.index(mostClearPaths)
 bestAstroid = astroidCoords[bestAstroidIndex]
 print("The best astroid had a clear view path to: ", mostClearPaths, " other astroids. Position: ", bestAstroid)
+
+#
+#   Part 2 - Complete vaporization by giant laser
+#
+def calcDistance(coord1, coord2):
+    x1, y1, x2, y2 = coord1[0], coord1[1], coord2[0], coord2[1]
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+def splitDictionary(dict):
+    first = {x: dict[x] for x in dict if x >= 90.0}
+    last = {x: dict[x] for x in dict if x < 90}
+    return first, last
+
+def findAstroidToVaporize(astroids, monitoringStation):
+    # In case only one astroid in array, then that one is the one to be vaporized
+    astroidToVaporize = astroids[0]
+    # But if there are more, we calculate which is closest to our monitoring station
+    if len(astroids) > 1:
+        currentDist = calcDistance(monitoringStation, astroidToVaporize)
+        for astroid in astroids[1:]:
+            # Find the closest astroid at this angle
+            dist = calcDistance(monitoringStation, astroid)
+            if dist < currentDist:
+                currentDist = dist
+                astroidToVaporize = astroid
+    return astroidToVaporize
+
+
+# Setup the monitoring station at the location of the astroid with the best view
+monitoringStation = bestAstroid
+
+vaporized = []
+# AstroidCoords need to be above 1, because 1 is our monitoring station
+while len(astroidCoords) > 1:
+    astroidsPerAngle = {}
+    for target in astroidCoords:
+        # Don't check self
+        if monitoringStation != target:
+            angle = calculateAngle(monitoringStation, target)
+            if angle not in astroidsPerAngle:
+                astroidsPerAngle[angle] = []
+            astroidsPerAngle[angle].append(target)
+
+    # A bit hacky, need to start at degrees = 90 and go through the angles sorted
+    firstAstroidsPerAngle, lastAstroidsPerAngle = splitDictionary(astroidsPerAngle)
+
+    astroidsToVaporize = []
+    # Start at angle = 90 and go clockwise
+    for angle in sorted(firstAstroidsPerAngle):
+        astroids = firstAstroidsPerAngle[angle]
+        astroidsToVaporize.append(findAstroidToVaporize(astroids, monitoringStation))
+    # Then go over angles from 0 to 90 (fourth quadrant)
+    for angle in sorted(lastAstroidsPerAngle):
+        astroids = lastAstroidsPerAngle[angle]
+        astroidsToVaporize.append(findAstroidToVaporize(astroids, monitoringStation))
+
+    vaporized.extend(astroidsToVaporize)
+    astroidCoords = [a for a in astroidCoords if a not in astroidsToVaporize]
+
+print(vaporized[199])
+print("Answer to part 2: ", vaporized[199][0] * 100 + vaporized[199][1])
